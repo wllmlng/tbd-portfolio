31732c0cfe6952cef928db4fcfba815aad83bbae
William Leung <william@chartmetric.com>
2024-10-08 22:03:50 -0700

    FE first page setup todo

--- fe-portfolio/src/App.tsx
+++ fe-portfolio/src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import Todo from './projects/Todo/Todo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <div className="App">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/todo">Todo</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/todo" element={<Todo />} />
+          </Routes>
+        </div>
+      </Router>
+
     </div>
   );
 }
---
+++ fe-portfolio/src/projects/Todo/Todo.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import styles from './Todo.module.scss';
+
+const Todo = () => {
+  const [todos, setTodos] = useState<string[]>([]); 
+  const [newTodo, setNewTodo] = useState<string>(''); 
+
+  
+  const addTodo = () => {
+    if (newTodo.trim()) {  
+      setTodos((prev)=>[...prev, newTodo]); 
+      setNewTodo(''); 
+    }
+  };
+
+  
+  const handleInputChange = (event:any) => {
+    setNewTodo(event.target.value);
+  };
+
+  return (
+    <div>
+        <h1>Simple Todo App</h1>
+        <input
+            type="text"
+            value={newTodo}
+            onChange={handleInputChange}
+            placeholder="Add a new task"
+        />
+        
+        <button onClick={addTodo}>Add Todo</button>
+        <ul>
+            {todos.map((todo, index) => (
+            <li className={styles.li} key={index}>{todo}</li>
+            ))}
+        </ul>
+    </div>
+  );
+}
+
+export default Todo;
