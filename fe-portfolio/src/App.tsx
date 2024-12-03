bf56f9935cad8b264e40b973f4eef1d74d630290
William Leung <william@chartmetric.com>
2024-12-02 22:30:44 -0800

    Practice class based js for nested checkboxes

--- fe-portfolio/src/App.tsx
+++ fe-portfolio/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Todo from './projects/Todo/Todo';
+import NestedCheckboxes from './projects/NestedCheckboxes/NestedCheckboxes';
 
 function App() {
   return (
@@ -13,11 +14,15 @@ function App() {
               <li>
                 <Link to="/todo">Todo</Link>
               </li>
+              <li>
+                <Link to="/nestedCheckboxes">NestedCheckboxes</Link>
+              </li>
             </ul>
           </nav>
 
           <Routes>
             <Route path="/todo" element={<Todo />} />
+            <Route path="/nestedCheckboxes" element={<NestedCheckboxes />} />
           </Routes>
         </div>
       </Router>
---
+++ fe-portfolio/src/projects/NestedCheckboxes/NestedCheckboxes.tsx
@@ -0,0 +1,82 @@
+import React, {useEffect} from 'react';
+import styles from './NestedCheckboxes.module.scss';
+
+const mockData = {
+    "VP of Engineering": {
+        "Engineering Manager": {
+            "Senior Engineer": {
+                "Intern1": {},
+                "Intern2": {},
+            },
+            "Junior Engineer": {}
+        },
+        "Engineering Manager 2": {
+            "Senior Engineer 2": {},
+            "Junior Engineer 2": {}
+        }
+    },
+    "Head of Product": {
+        "Product Manager1": {},
+        "Product Manager2": {},
+    }
+}
+
+class NestedCheckboxesClass {
+    data: Record<string, unknown>;
+    root: string;
+
+    constructor(data:Record<string, unknown>, root: string){
+        this.data = data;
+        this.root = root;
+    }
+    createList(data: Record<string, any>){
+        const ul = document.createElement('ul');
+        for(const key of Object.keys(data)){
+            
+            const li = document.createElement('li');
+            li.innerText = key;
+            const label = document.createElement('label');
+            
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            
+            label.appendChild(checkbox);
+            label.appendChild(li);
+            ul.appendChild(label);
+
+            if(Object.keys(data[key]).length > 0){
+                const nestedList = this.createList(data[key]);
+                li.appendChild(nestedList);
+            }
+        }
+
+
+
+        return ul
+    };
+    addCheckboxesListeners(){};
+    updateParentCheckbox(){};
+    getSelectionOptions(){};
+    init(){
+        const container = document.getElementById(this.root);
+        const list = this.createList(this.data);
+        if(container){
+            container.appendChild(list);
+            console.log('container', container);        
+        }
+    }
+}
+const NestedCheckboxes = () => {
+    
+    useEffect(() => {
+        const nestedCheckboxes = new NestedCheckboxesClass(mockData, 'nestedCheckboxes');
+        nestedCheckboxes.init();
+    },[])
+
+
+
+    return (
+        <div id="nestedCheckboxes" className={styles.nestedCheckboxes}>Vanilla Nested Checkboxes</div>
+    )
+}
+export default NestedCheckboxes;
